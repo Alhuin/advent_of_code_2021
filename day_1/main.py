8db1437c51a127a3a2519d3951074add55de240f
Alhuin <jjanin-r@student.42lyon.fr>
2021-12-01 23:36:33 +0100

    naming & type hints

--- day_1/main.py
+++ day_1/main.py
@@ -1,16 +1,14 @@
-def ex_01(data):
+def ex_01(data: list[int]) -> int:
     return sum(1 for i in range(len(data) - 1) if data[i] < data[i + 1])
 
 
-def ex_02(data, n):
+def ex_02(data: list[int], n: int) -> int:
     return ex_01([sum(data[i:n + i]) for i in range(len(data) - n + 1)])
 
 
 if __name__ == '__main__':
     with open('input.txt') as file:
-        data = file.read().split('\n')
+        dataset = [int(value) for value in file.read().split('\n')[:-1]]
 
-    data = [int(value) for value in data[:-1]]
-
-    ex_01(data)
-    print(ex_02(data, 3))
+    print(ex_01(dataset))
+    print(ex_02(dataset, 3))
