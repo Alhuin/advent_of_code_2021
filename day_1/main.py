c50eb6ba60c97ddc122adf573d749cc63f557aa3
Alhuin <jjanin-r@student.42lyon.fr>
2021-12-02 00:23:32 +0100

    encoding in open, naming

--- day_1/main.py
+++ day_1/main.py
@@ -2,12 +2,12 @@ def ex_01(data: list[int]) -> int:
     return sum(1 for i in range(len(data) - 1) if data[i] < data[i + 1])
 
 
-def ex_02(data: list[int], n: int) -> int:
-    return ex_01([sum(data[i:n + i]) for i in range(len(data) - n + 1)])
+def ex_02(data: list[int], window: int) -> int:
+    return ex_01([sum(data[i:window + i]) for i in range(len(data) - window + 1)])
 
 
 if __name__ == '__main__':
-    with open('input.txt') as file:
+    with open('input.txt', encoding="utf-8") as file:
         dataset = [int(value) for value in file.read().split('\n')[:-1]]
 
     print(ex_01(dataset))
